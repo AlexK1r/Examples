f265be06066d5e16f935c1bc4d861b8aa87e5018
Пользователь <ya@ya.ru>
2022-10-02 17:25:26 +0700

    add Example 004 Div

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("ведите делимое");
+double numberA = Int32.Parse(Console.ReadLine());
+Console.WriteLine("введите частное");
+double numberB = Int32.Parse(Console.ReadLine());
+double Div = numberA / numberB;
+Console.WriteLine(Div);
