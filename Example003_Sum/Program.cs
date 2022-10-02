006c37fbc4881307637b70bae5e79d17395aa899
Пользователь <ya@ya.ru>
2022-10-02 18:18:06 +0700

    add Random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,7 +1,11 @@
 ﻿//сумма двух целых чисел
-Console.WriteLine("введите первое число");
-int numberA = Int32.Parse(Console.ReadLine());
-Console.WriteLine("введите втрое число");
-int numberB = Int32.Parse(Console.ReadLine());
+//Console.WriteLine("введите первое число");
+//int numberA = Int32.Parse(Console.ReadLine());
+int numberA = new Random().Next(1,10); //значения будет от 1 до 9 т.к. значение max-1
+Console.WriteLine(numberA);
+//Console.WriteLine("введите втрое число");
+//int numberB = Int32.Parse(Console.ReadLine());
+int numberB = new Random().Next(1,15);
+Console.WriteLine(numberB);
 int sum = numberA + numberB;
 Console.WriteLine(sum);
