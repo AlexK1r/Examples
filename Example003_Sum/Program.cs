361d501cc2fd987ea9752269ed0d1d0467c09f60
Пользователь <ya@ya.ru>
2022-10-02 17:06:59 +0700

    add Example 003 Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,7 @@
+﻿//сумма двух целых чисел
+Console.WriteLine("введите первое число");
+int numberA = Int32.Parse(Console.ReadLine());
+Console.WriteLine("введите втрое число");
+int numberB = Int32.Parse(Console.ReadLine());
+int sum = numberA + numberB;
+Console.WriteLine(sum);
