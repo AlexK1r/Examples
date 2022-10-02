382f87837582f1ac1d7f23282740d1d3635f0e2d
Пользователь <ya@ya.ru>
2022-10-02 18:30:23 +0700

    add if else

---
+++ Example005_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string UserName = Console.ReadLine();
+
+if (UserName == "Маша" )
+{
+    Console.Write("Ура, это же суппер польлзователь Маша");
+}
+else 
+{
+    Console.Write("Привет ");
+    Console.Write(UserName);
+}
